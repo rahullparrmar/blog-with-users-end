74b6704f242c05d6487ee15dba9eec52295a94b4
rahullparrmar <105049136+rahullparrmar@users.noreply.github.com>
2023-01-01 11:58:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
-WTF_CSRF_CHECK_DEFAULT = False
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
@@ -24,6 +23,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 
 @login_manager.user_loader
